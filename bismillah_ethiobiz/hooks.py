adf66fcfe8a0fa3a53f2dc1779034ad8e23b207c
Bizit <bizit@ethiobiz.et>
2026-07-27 18:05:12 +0300

    fix: remove /dagu route redirect to lms so Web Page renders correctly

--- bismillah_ethiobiz/hooks.py
+++ bismillah_ethiobiz/hooks.py
@@ -67,8 +67,7 @@ website_route_rules = [
     {"from_route": "/helpdesk/<path:app_path>", "to_route": "helpdesk"},
     {"from_route": "/lms", "to_route": "lms"},
     {"from_route": "/lms/<path:app_path>", "to_route": "lms"},
-    {"from_route": "/dagu", "to_route": "lms"},
-    {"from_route": "/dagu/<path:app_path>", "to_route": "lms"},
+
 ]
 
 # Home page (optional override)
